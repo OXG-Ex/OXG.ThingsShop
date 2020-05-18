951775cb63fe3837ecec3a10b9e827f61714ade8
Назаренко Семён <semes212@gmail.com>
2020-05-18 17:09:13 +0300

    Добавлены мета-данные модели и создан CRUD-контроллер методом скаффолдинга.

--- OXG.ThingsShop/Models/Product.cs
+++ OXG.ThingsShop/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -10,29 +11,32 @@ namespace OXG.ThingsShop.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Это поле обязательно для заполнения")]
         [Display(Name = "Намименование")]
         public string Name { get; set; }//Наименование
 
-        [Required]
+        [Required(ErrorMessage = "Это поле обязательно для заполнения")]
+        [Display(Name = "Количество в упаковке")]
+        [Range(0, 999, ErrorMessage = "Значение не может быть меньше 0 и больше 999")]
+        public int? Count { get; set; }//Кол-во
+
+        [Required(ErrorMessage ="Это поле обязательно для заполнения")]
         [Display(Name = "Стоимость")]
-        [DataType(DataType.Currency)]
-        public decimal Price { get; set; }//Стоимость
+        [DataType(DataType.Currency, ErrorMessage = "Это поле обязательно для заполнения")]
+        public decimal? Price { get; set; }//Стоимость
 
         [Display(Name = "Описание")]
         [DataType(DataType.MultilineText)]
-        public string Description { get; set; }//Описание, необязательное поле
+        public string? Description { get; set; }//Описание, необязательное поле
 
         [Display(Name = "Производитель")]
-        public string Manufacturer { get; set; }//Производитель, Необязательное поле
+        public string? Manufacturer { get; set; }//Производитель, Необязательное поле
 
         [Display(Name = "Цвет")]
-        public string Color { get; set; }//Цвет, Необязательное поле
-
-        [Display(Name = "Кол-во")]
-        public int Count { get; set; }//Кол-во, Необязательное поле
+        public string? Color { get; set; }//Цвет, Необязательное поле
 
         [Display(Name = "Год выпуска")]
-        public string Year { get; set; }//Год выпуска, Необязательное поле
+        [Range(1960, 2020, ErrorMessage = "Значение не может быть меньше 1960 и больше 2020")]
+        public int? Year { get; set; }//Год выпуска, Необязательное поле
     }
 }
